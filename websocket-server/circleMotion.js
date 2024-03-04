b2f05af78baf13277f2f0ed7f9351d25ba384041
yuu <crosscore.g@gmail.com>
2024-03-04 17:28:51 +0900

    update circleMotion.js

--- websocket-server/circleMotion.js
+++ websocket-server/circleMotion.js
@@ -1,12 +1,12 @@
 // websocket-server/circleMotion.js
 
-let circle = { x: -50, y: 300, velocity: 1 };
+let circle = { x: -100, y: 300, velocity: 1 };
 const circleLifetime = 10000;
-const circleRadius = 50;
+const circleRadius = 100;
 
 function createCircle(totalWidth) {
   if (!circle || circle.x > totalWidth) {
-    circle = { x: -50, y: 300, velocity: 1 };
+    circle = { x: -100, y: 300, velocity: 1 };
     setTimeout(() => circle = null, circleLifetime);
   }
 }
@@ -19,27 +19,27 @@ function updateCircles() {
 
 function sendCirclePositions(wss, isOpen, clientWidths, clients) {
   if (!circle) return;
-  let cumulativeWidth = 0;
+  let cumulativeWidth = 0; // cumulative width of all connected clients
 
-  wss.clients.forEach(client => {
-    const clientId = clients.get(client); // get the client ID by clients map
+  wss.clients.forEach((client, index) => {
+    const clientId = clients.get(client); // get the client ID from the clients(Map) object
     if (!clientId || !clientWidths.has(clientId) || !isOpen(client)) return;
 
     const clientWidth = clientWidths.get(clientId);
-    if (circle.x >= cumulativeWidth && circle.x < cumulativeWidth + clientWidth) {
+    if (circle.x + circleRadius >= cumulativeWidth && circle.x - circleRadius < cumulativeWidth + clientWidth) {
       client.send(JSON.stringify({
         type: 'updateCircle',
         data: { x: circle.x - cumulativeWidth, y: circle.y },
       }));
-    } else { // if the circle is outside the client's viewport, send a position that will make it disappear
+    } else {
       client.send(JSON.stringify({
         type: 'updateCircle',
         data: { x: -100, y: circle.y },
       }));
     }
-
-    cumulativeWidth += clientWidth;
+    cumulativeWidth += clientWidth; // update the cumulative width for the next iteration
   });
 }
+
 
 module.exports = { createCircle, updateCircles, sendCirclePositions };
