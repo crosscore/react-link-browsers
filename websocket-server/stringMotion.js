02d083e00b4a4be50e0b6c28332b02c7cc47dce8
yuu <crosscore.g@gmail.com>
2024-03-13 13:09:01 +0900

    update stringMotion.js

--- websocket-server/stringMotion.js
+++ websocket-server/stringMotion.js
@@ -2,11 +2,10 @@
 
 let piDigits = [];
 let nextPiDigitId = 0;
-const digitWidth = 20;
+const fontSize = 360; // App.tsxから取得したフォントサイズを使用
 const digitVelocity = 6;
 
-const piString =
-  "Thank you for your attention! This is the first 100 digits of pi: 3.1415926535 8979323846 2643383279 5028841971 6939937510 5820974944 5923078164 0628620899 8628034825 3421170679 ";
+const piString = "Thank you for your attention! This is the first 100 digits of pi: 3.1415926535 8979323846 2643383279 5028841971 6939937510 5820974944 5923078164 0628620899 8628034825 3421170679 ";
 let currentStringIndex = 0;
 
 function createPiDigit() {
@@ -17,25 +16,24 @@ function createPiDigit() {
   const newDigit = {
     id: nextPiDigitId++,
     digit: digit,
-    x: -digitWidth * (piDigits.length + 1), // 文字の初期位置を調整
+    x: -fontSize, // 画面外からフォントサイズ分だけ左にオフセットして文字が表示され始めるように調整
     y: 200,
     velocity: digitVelocity,
   };
   piDigits.push(newDigit);
 }
 
-function updatePiDigitsPosition(totalWidth) {
-  piDigits.forEach((digit) => {
-    digit.x += digit.velocity;
-  });
-  // 文字が画面の端を超えた場合に配列から削除
-  piDigits = piDigits.filter((digit) => digit.x <= totalWidth + digitWidth);
-}
-
 function generatePiDigits(interval = 500) {
   setInterval(() => {
     createPiDigit();
   }, interval);
+}
+
+function updatePiDigitsPosition(totalWidth) {
+  piDigits.forEach((digit) => {
+    digit.x += digit.velocity;
+  });
+  piDigits = piDigits.filter((digit) => digit.x <= totalWidth + fontSize); // fontSizeを使用してフィルタリング条件を調整
 }
 
 function sendPiDigitPositions(wss, isOpen, clientWidths, clients) {
@@ -46,7 +44,7 @@ function sendPiDigitPositions(wss, isOpen, clientWidths, clients) {
     const clientWidth = clientWidths.get(clientId);
     piDigits.forEach((digit) => {
       const adjustedX = digit.x - cumulativeWidth;
-      if (adjustedX + digitWidth > 0 && adjustedX < clientWidth) {
+      if (adjustedX + fontSize > 0 && adjustedX < clientWidth) { // fontSizeを使用して条件を調整
         client.send(JSON.stringify({
           type: "updatePiDigit",
           data: {
